aab13bf1b802190362e201d041990aa62dd8061b
HAlGhanim <imzaji98@gmail.com>
2025-04-06 11:12:31 +0300

    Done with the task

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,12 @@
+import { ContentComponent } from '../content/content.component';
+import { SiderbarComponent } from '../siderbar/siderbar.component';
+import { HeaderComponent } from './../header/header.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, SiderbarComponent, ContentComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
